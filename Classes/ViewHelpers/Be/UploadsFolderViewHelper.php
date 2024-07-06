37d08573510cec6f414aea318fcc68f15ea60896
honsa.ch <straka@8038.ch>
2024-07-06 08:58:58 +0200

    Get uploadFolder from typoscript setup

--- Classes/ViewHelpers/Be/UploadsFolderViewHelper.php
+++ Classes/ViewHelpers/Be/UploadsFolderViewHelper.php
@@ -5,6 +5,8 @@ namespace In2code\Powermail\ViewHelpers\Be;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 
 /**
  * Class UploadsFolderViewHelper
@@ -25,6 +27,11 @@ class UploadsFolderViewHelper extends AbstractViewHelper
      */
     public function render(): bool
     {
+        $configurationManager = GeneralUtility::makeInstance(ConfigurationManager::class);
+        $fullTypoScriptArray = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+
+        $folder = $fullTypoScriptArray['plugin.']['tx_powermail.']['settings.']['setup.']['misc.']['file.']['folder'] ?? $this->folder;
+        
         return file_exists(GeneralUtility::getFileAbsFileName($this->folder));
     }
 }
